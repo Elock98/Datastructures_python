7dfb9fd1c0a2022d42fe1678838567eeb74db584
Emil Lock <emiloc-0@student.ltu.se>
2022-08-13 16:58:04 +0200

    Added object creation

--- hash_table/hash_table.py
+++ hash_table/hash_table.py
@@ -1,9 +1,20 @@
-
+from typing import Callable
 
 class HashTable:
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, hash_function:Callable[[], int],
+                    table_length:int, chaining:bool=False) -> None:
+        # Argument validation
+        assert callable(hash_function), "Hash function needs to be callable!"
+        assert isinstance(table_length, int), "Table length needs to be of type int!"
+        assert table_length > 0, "Table length needs to be larger than 0!"
+        assert isinstance(chaining, bool), "Chaning needs to be of type bool!"
+
+        # Setting instance variables
+        self._hash_function = hash_function
+        self._table_langth = table_length
+        self._chaining = chaining
+        self._hash_table = []
 
     def insert(self, key, value) -> None:
         pass
--- hash_table/test_hash_table.py
+++ hash_table/test_hash_table.py
@@ -1,9 +1,62 @@
 import unittest
 
+from hash_table import HashTable
 from unittest.mock import patch
 
 
 class TestHashTable(unittest.TestCase):
+
+#-------------------------------------------------------------------#
+
+    def test_creation_bad_function(self):
+        # Given / When
+        with self.assertRaises(Exception) as ex:
+            table = HashTable("foo", 5)
+
+        # Then
+        self.assertEqual(str(ex.exception),
+                        "Hash function needs to be callable!")
+
+    def test_creation_bad_table_lenght_type(self):
+        # Given / When
+        with self.assertRaises(Exception) as ex:
+            table = HashTable(lambda:5, "foo")
+
+        # Then
+        self.assertEqual(str(ex.exception),
+                        "Table length needs to be of type int!")
+
+    def test_creation_bad_table_lenght(self):
+        # Given / When
+        with self.assertRaises(Exception) as ex:
+            table = HashTable(lambda:5, -123)
+
+        # Then
+        self.assertEqual(str(ex.exception),
+                        "Table length needs to be larger than 0!")
+
+    def test_creation_bad_chaining(self):
+        # Given / When
+        with self.assertRaises(Exception) as ex:
+            table = HashTable(lambda:5, 5, "foo")
+
+        # Then
+        self.assertEqual(str(ex.exception),
+                        "Chaning needs to be of type bool!")
+
+    def test_creation(self):
+        # Given
+        fun = lambda:5
+
+        # When
+        table = HashTable(fun, 5, True)
+
+        # Then
+        self.assertTrue(callable(table._hash_function))
+        self.assertEqual(table._hash_function, fun)
+        self.assertEqual(table._table_langth, 5)
+        self.assertEqual(table._chaining, True)
+        self.assertEqual(table._hash_table, [])
 
 #-------------------------------------------------------------------#
 
