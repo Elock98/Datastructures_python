fc0a8e066d076eefed1b576ffaf29293334e9e2f
Emil Lock <emiloc-0@student.ltu.se>
2022-08-13 16:11:20 +0200

    Init commit for hash table

---
+++ hash_table/hash_table.py
@@ -0,0 +1,19 @@
+
+
+class HashTable:
+
+    def __init__(self) -> None:
+        pass
+
+    def insert(self, key, value) -> None:
+        pass
+
+    def _hash(self, key) -> int:
+        pass
+
+    def get(self, key):
+        pass
+
+    def remove(self, key):
+        pass
+    
