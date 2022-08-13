f84df07335f62fa5f9768402e9940d3cfafc5385
Emil Lock <emiloc-0@student.ltu.se>
2022-08-13 19:16:21 +0200

    Added comments

--- hash_table/hash_table.py
+++ hash_table/hash_table.py
@@ -21,17 +21,20 @@ class HashTable:
             self._hash_table = [None] * self._table_langth
 
     def insert(self, key, value) -> None:
+        '''Inserts (key, value) into the hash table'''
         if self._chaining:
             self._insert_chaining(key, value)
         else:
             self._insert_linear_probing(key, value)
 
     def _insert_chaining(self, key, value) -> None:
+        '''Insertion for chaining'''
         index = self._hash(key)
 
         self._hash_table[index].append((key, value))
 
     def _insert_linear_probing(self, key, value) -> None:
+        '''Insertion for linear probing'''
         index = start_index = self._hash(key)
 
         while True:
@@ -47,15 +50,18 @@ class HashTable:
                 raise ValueError("Hash table is full!")
 
     def _hash(self, key) -> int:
+        '''Runs the hashing function given at creation'''
         return self._hash_function(key)
 
     def get(self, key):
+        '''Returns the value stored with key'''
         if self._chaining:
             return self._get_chaining(key)
         else:
             return self._get_linear_probing(key)
 
     def _get_chaining(self, key):
+        '''Gets from chained hash table'''
         index = self._hash(key)
 
         for entry in self._hash_table[index]:
@@ -65,6 +71,7 @@ class HashTable:
         raise ValueError("Key not in hash table!")
 
     def _get_linear_probing(self, key):
+        '''Gets from linear hash table'''
         index = start_index = self._hash(key)
 
         while True:
@@ -80,12 +87,14 @@ class HashTable:
                 raise ValueError("Key not in hash table!")
 
     def remove(self, key):
+        '''Removes entry marked by key from the hash table'''
         if self._chaining:
             self._remove_chaining(key)
         else:
             self._remove_linear(key)
 
     def _remove_chaining(self, key):
+        '''Removes from chained hash table'''
         index = self._hash(key)
 
         for pos, entry in enumerate(self._hash_table[index]):
@@ -96,6 +105,7 @@ class HashTable:
         raise ValueError("Key not in hash table!")
 
     def _remove_linear(self, key):
+        '''Removes from linear hash table'''
         index = start_index = self._hash(key)
 
         while True:
@@ -112,4 +122,5 @@ class HashTable:
                 raise ValueError("Key not in hash table!")
 
     def print_hash_table(self):
+        '''Prints the hash table'''
         print(self._hash_table)
