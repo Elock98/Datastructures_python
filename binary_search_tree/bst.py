d5f58924f980ca9757a2bf896150dfac31cfa84b
Emil Lock <emiloc-0@student.ltu.se>
2022-08-14 12:38:17 +0200

    Added setters for node

--- binary_search_tree/bst.py
+++ binary_search_tree/bst.py
@@ -9,13 +9,22 @@ class _Node:
         self._parent = parent
 
     def set_left_child(self, child:_Node=None) -> None:
-        pass
+        assert isinstance(child, _Node) or\
+                child == None, "Bad child type!"
+
+        self._left_child = child
 
     def set_right_child(self, child:_Node=None) -> None:
-        pass
+        assert isinstance(child, _Node) or\
+                child == None, "Bad child type!"
+
+        self._right_child = child
 
     def set_parent(self, parent:_Node=None) -> None:
-        pass
+        assert isinstance(parent, _Node) or\
+                parent == None, "Bad parent type!"
+
+        self._parent = parent
 
     def get_left_child(self) -> _Node:
         pass
--- binary_search_tree/test_bst.py
+++ binary_search_tree/test_bst.py
@@ -30,7 +30,155 @@ class TestNode(unittest.TestCase):
         self.assertEqual(node._right_child, None)
         self.assertEqual(node._parent, root_node)
 
+#-------------------------------------------------------------------#
 
+    def test_set_left_child(self):
+        # Given
+        node = _Node()
+        child_node = _Node(node)
+
+        # When
+        node.set_left_child(child_node)
+
+        # Then
+        self.assertEqual(node._left_child, child_node)
+        self.assertEqual(child_node._parent, node)
+
+    def test_set_left_child_to_none(self):
+        # Given
+        node = _Node()
+        child_node = _Node(node)
+
+        # When
+        node.set_left_child(None)
+
+        # Then
+        self.assertEqual(node._left_child, None)
+        self.assertEqual(child_node._parent, node)
+
+    def test_set_left_child_no_param(self):
+        # Given
+        node = _Node()
+        child_node = _Node(node)
+
+        # When
+        node.set_left_child()
+
+        # Then
+        self.assertEqual(node._left_child, None)
+        self.assertEqual(child_node._parent, node)
+
+    def test_set_left_child_bad_param(self):
+        # Given
+        node = _Node()
+        child_node = _Node(node)
+
+        # When
+        with self.assertRaises(Exception) as ex:
+            node.set_left_child("foo")
+
+        # Then
+        self.assertEqual(str(ex.exception), "Bad child type!")
+        self.assertEqual(node._left_child, None)
+
+#-------------------------------------------------------------------#
+
+    def test_set_right_child(self):
+        # Given
+        node = _Node()
+        child_node = _Node(node)
+
+        # When
+        node.set_right_child(child_node)
+
+        # Then
+        self.assertEqual(node._right_child, child_node)
+        self.assertEqual(child_node._parent, node)
+
+    def test_set_right_child_to_none(self):
+        # Given
+        node = _Node()
+        child_node = _Node(node)
+
+        # When
+        node.set_right_child(None)
+
+        # Then
+        self.assertEqual(node._right_child, None)
+        self.assertEqual(child_node._parent, node)
+
+    def test_set_right_child_no_param(self):
+        # Given
+        node = _Node()
+        child_node = _Node(node)
+
+        # When
+        node.set_right_child()
+
+        # Then
+        self.assertEqual(node._right_child, None)
+        self.assertEqual(child_node._parent, node)
+
+    def test_set_right_child_bad_param(self):
+        # Given
+        node = _Node()
+        child_node = _Node(node)
+
+        # When
+        with self.assertRaises(Exception) as ex:
+            node.set_left_child("foo")
+
+        # Then
+        self.assertEqual(str(ex.exception), "Bad child type!")
+        self.assertEqual(node._left_child, None)
+
+#-------------------------------------------------------------------#
+
+    def test_set_parent(self):
+        # Given
+        parent = _Node()
+        child = _Node()
+        parent.set_left_child(child)
+
+        # When
+        child.set_parent(parent)
+
+        # Then
+        self.assertEqual(child._parent, parent)
+
+    def test_set_parent_to_none(self):
+        # Given
+        child = _Node()
+
+        # When
+        child.set_parent(None)
+
+        # Then
+        self.assertEqual(child._parent, None)
+
+    def test_set_parent_no_param(self):
+        # Given
+        child = _Node()
+
+        # When
+        child.set_parent()
+
+        # Then
+        self.assertEqual(child._parent, None)
+
+    def test_set_parent_bad_param(self):
+        # Given
+        child = _Node()
+
+        # When
+        with self.assertRaises(Exception) as ex:
+            child.set_parent("foo")
+
+        # Then
+        self.assertEqual(str(ex.exception), "Bad parent type!")
+        self.assertEqual(child._parent, None)
+
+#-------------------------------------------------------------------#
 
 class TestBST(unittest.TestCase):
 
