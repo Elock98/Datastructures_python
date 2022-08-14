7e8ccc4dc1f5867c85359fd9393c92f7e3420a95
Emil Lock <emiloc-0@student.ltu.se>
2022-08-14 11:55:23 +0200

    Added files for Binary Search Tree

---
+++ binary_search_tree/bst.py
@@ -0,0 +1,15 @@
+
+
+class BST:
+
+    def __init__(self) -> None:
+        pass
+
+    def insert(self):
+        pass
+
+    def find(self):
+        pass
+
+    def delete(self):
+        pass
---
+++ binary_search_tree/test_bst.py
@@ -0,0 +1,14 @@
+import imp
+import unittest
+
+from bst import BST
+from unittest.mock import patch
+
+class TestBST(unittest.TestCase):
+
+    def test_create(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
